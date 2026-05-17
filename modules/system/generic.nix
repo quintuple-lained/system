7ba33060c806ca1a41618cc75aa6e43433c1a725
quintuple-lained <quintuple-lained@proton.me>
2026-05-17 11:54:46 +0200

    more additions

--- modules/system/generic.nix
+++ modules/system/generic.nix
@@ -9,7 +9,22 @@
     ];
   };
 
-  i18n.defaultLocale = "en_GB.UTF-8";
+  # you might not like it but this is what peak formatting looks like
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+    };
+  };
   time.timeZone = "Europe/Berlin";
 
   services.resolved.enable = true;
